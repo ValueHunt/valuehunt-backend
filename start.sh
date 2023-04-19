73c65f2beaf387f29eb01a0551468a2522eca73d
A7Coder <a7coder@gmail.com>
2023-04-19 09:19:58 +0530

    Removed start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
-
-apt install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libglib2.0-0 libgconf-2-4
-apt install -y google-chrome-stable
-
-python -m gunicorn --bind=0.0.0.0 --timeout 600 app:app
